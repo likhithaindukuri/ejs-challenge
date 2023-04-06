51d8821d648bc3bea714f4d4f6b6480cbff1f221
Indukuri Likhitha <likhithaindukuri07@gmail.com>
2023-04-06 19:28:12 +0530

    Modified

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.static("public"));
 
 
 app.get("/",function(req,res){
-  res.render("home", {startingContent:homeStartingContent});
+  res.render("home", {startingContent:homeStartingContent})
 });
 
 
