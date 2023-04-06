4bd3cdcd9422024239cf5070469de60fcbe03aa8
Indukuri Likhitha <likhithaindukuri07@gmail.com>
2023-04-06 19:36:23 +0530

    Modified

--- app.js
+++ app.js
@@ -22,7 +22,9 @@ app.get("/",function(req,res){
   res.render("home", {startingContent:homeStartingContent})
 });
 
-
+app.get("/about",function(req,res){
+  res.render("about",{aboutContent: aboutContent});
+});
 
 
 
